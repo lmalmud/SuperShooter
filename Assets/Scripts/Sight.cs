f65c2dcda86869edd5edfb4cf995334b69d50533
lmalmud <lmalmud2@jh.edu>
2025-09-26 13:20:07 -0400

    the enemy detects nearby player

--- Assets/Scripts/Sight.cs
+++ Assets/Scripts/Sight.cs
@@ -9,14 +9,32 @@ public class Sight : MonoBehaviour
     public float angle;
     public LayerMask objectsLayers;
     public LayerMask obstaclesLayers;
+    public Collider detectedObject;
 
     void Update()
     {
+        // returns an array of colliders found within the sphere
+        // note the first parameter is the position of the AI object, not the actual enemy
         Collider[] colliders = Physics.OverlapSphere(transform.position, distance, objectsLayers);
+
+        detectedObject = null;
 
         for (int i = 0; i < colliders.Length; i++)
         {
-            colliders collider = colliders[i];
+            Collider collider = colliders[i];
+
+            Vector3 directionToController = Vector3.Normalize(collider.bounds.center - transform.position);
+
+            float angleToCollider = Vector3.Angle(transform.forward, directionToController);
+
+            if (angleToCollider < angle)
+            {
+                if (!Physics.Linecast(transform.position, collider.bounds.center, (int)obstaclesLayers))
+                {
+                    detectedObject = collider;
+                    break;
+                }
+            }
         }
     }
 }
