3958c601da64a5fd896d7a6fb64d5b84093c5844
lmalmud <lmalmud2@jh.edu>
2025-09-25 23:35:59 -0400

    finished chapter 7

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
 
+    private Rigidbody rigidbody;
     public float speed;
     public float rotationSpeed;
     private Vector2 movementValue;
@@ -21,32 +22,25 @@ public class PlayerMovement : MonoBehaviour
         lookValue = value.Get<Vector2>().x * rotationSpeed;
     }
 
+
     public void Awake()
     {
         Cursor.visible = false; // Make cursor invisible
         Cursor.lockState = CursorLockMode.Locked; // Lock so that the cursor is in the middle of the screen
+
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        transform.Translate(
+        rigidbody.AddRelativeForce(
             movementValue.x * Time.deltaTime,
             0,
             movementValue.y * Time.deltaTime
         );
 
-        transform.Rotate(0, lookValue * Time.deltaTime, 0);
-
-        /* Old version - before input mapping
-        float mouseX = Input.GetAxis("Mouse X");
-        transform.Rotate(0, mouseX * rotationSpeed * Time.deltaTime, 0); // parameters are in order x, y, z
-
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) { transform.Translate(0, 0, speed * Time.deltaTime); }
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) { transform.Translate(-speed * Time.deltaTime, 0, 0); }
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) { transform.Translate(0, 0, -speed * Time.deltaTime); }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) { transform.Translate(speed * Time.deltaTime, 0, 0);  }
-        */
+        rigidbody.AddRelativeTorque(0, lookValue * Time.deltaTime, 0);
     }
 }
