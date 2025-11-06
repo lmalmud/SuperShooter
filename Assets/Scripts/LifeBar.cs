6a6334e747e05bdd8ad7c29c795dacb71fe01998
lmalmud <lmalmud2@jh.edu>
2025-11-06 09:08:09 -0500

    p 481

---
+++ Assets/Scripts/LifeBar.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeBar : MonoBehaviour
+{
+    public Life targetLife; // the life to be monitored
+}
